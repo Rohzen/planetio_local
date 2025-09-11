dbd9011c75959c373501878f2db98e11b6cbe553
Roberto Zanardo <rohzen1@gmail.com>
2025-09-11 23:26:53 +0200

    restore

--- planetio/__manifest__.py
+++ planetio/__manifest__.py
@@ -10,21 +10,15 @@
         # 'views/job_views.xml',
         'views/template_views.xml',
         'views/wizard_views.xml',
-        # 'views/wizard_oauth.xml',
         'data/eudr_stages.xml',
         'data/eudr_params.xml',
         'data/seed_template.xml',
         'data/sequence.xml',
         'data/gfw_api_params.xml',
-        'views/otp_verification_wizard.xml',
-        # 'views/planetio_assets.xml',
     ],
     # 'external_dependencies': {
     #     'python' : ['pandas', 'requests', 'google-generativeai'],
     # },
-    # 'qweb': [
-    #     'static/src/xml/geojson_map.xml',  # QWeb files go here in v14
-    # ],
     'installable': True,
     'application': True,
 }
--- planetio/wizards/__init__.py
+++ planetio/wizards/__init__.py
@@ -1,6 +1,1 @@
-from . import (
-    import_wizard,
-    otp_verification_wizard,
-)
-
-from . import oauth_test_wizard
+from . import import_wizard
