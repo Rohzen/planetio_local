d3010201967b923a7b1de568a47848dab3303479
Roberto Zanardo <rohzen1@gmail.com>
2025-09-16 22:14:28 +0200

    Clarify seed template comment after alias removal

--- planetio/models/__init__.py
+++ planetio/models/__init__.py
@@ -3,5 +3,4 @@ from . import eudr_deforestation
 from . import res_config_settings
 from . import excel_import_template
 from . import excel_import_service
-from . import excel_header_matcher
 from . import excel_transformers
